6ca6edc38d4d1b1e5bfdbdcea3e636db026eefcd
Luqman <luqdek@gmail.com>
2021-07-01 04:50:13 +0100

    add empty cipher method

--- csharp-ceaser-cipher/Program.cs
+++ csharp-ceaser-cipher/Program.cs
@@ -4,6 +4,14 @@ namespace csharp_ceaser_cipher
 {
     class Program
     {
+
+        static string CeaserCipher(string input, int shift)
+        {
+            string shifted = "";
+
+            return shifted;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
