eb8a4be1d08197b9e3a4961d0fdd75cb9d10ce12
Luqman <luqdek@gmail.com>
2021-07-01 04:54:33 +0100

    add for loop to iterate through each letter

--- csharp-ceaser-cipher/Program.cs
+++ csharp-ceaser-cipher/Program.cs
@@ -8,7 +8,10 @@ namespace csharp_ceaser_cipher
         static string CeaserCipher(string input, int shift)
         {
             char[] InputArray = input.ToCharArray();
-            
+            for(int i = 0; i < InputArray.Length; i++)
+            {
+
+            }
             return InputArray.ToString();
         }
 
