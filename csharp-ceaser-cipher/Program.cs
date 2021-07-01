9574e1bd2590b92e1daba3493215d1b93107d4fd
Luqman <luqdek@gmail.com>
2021-07-01 05:05:37 +0100

    call simplifyshift method in ceasercipher method

--- csharp-ceaser-cipher/Program.cs
+++ csharp-ceaser-cipher/Program.cs
@@ -25,6 +25,7 @@ namespace csharp_ceaser_cipher
         static string CeaserCipher(string input, int shift)
         {
             char[] InputArray = input.ToCharArray();
+            shift = SimplifyShift(shift);
             for(int i = 0; i < InputArray.Length; i++)
             {
 
